d476c3c522ec0137ad1545de969fe0f05adefd52
Wei Pan <wp2251@columbia.edu>
2020-07-06 14:12:14 -0400

    User Model Created

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
 const express = require('express')
-const app =  new express()
+const connectDB = require('./config/db')
 
+const app =  express()
 const PORT = process.env.PORT || 5000
+
+// Connect database
+connectDB()
+
+// Init middleware
+app.use(express.json({ extended: false }))
+
+// Define routes
+app.use('/api/users', require('./routes/api/users'))
+app.use('/api/auth', require('./routes/api/auth'))
+app.use('/api/profile', require('./routes/api/profile'))
+app.use('/api/posts', require('./routes/api/posts'))
+
 
 app.get('/', (req, res) => {
     res.send('API running')
