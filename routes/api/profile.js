eeae0366bcb7469d3e174e4d45ec73581c363e85
Wei Pan <wp2251@columbia.edu>
2020-07-07 18:29:56 -0400

    Profile finished and add github repos

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const request = require('request')
+const axios = require('axios')
 const config = require('config')
 const auth = require('../../middleware/auth')
 const { check, validationResult } = require('express-validator')
@@ -302,24 +302,17 @@ router.delete('/education/:edu_id', auth, async (req, res) => {
 // @desc    Get user repos from Github
 // @access  Public
 
-router.get('/github/:username', (req, res) => {
+router.get('/github/:username', async (req, res) => {
     try {
-        const options = {
-            uri: `https://api/github.com/users/${ req.params.username }/repos?per_page=5&
-            sort=created:asc&client_id=${ config.get('githubClientId') }&client_secret=
-            ${ config.get('githubSecret') }`,
-            method: 'GET',
-            headers: { 'user-agent': 'node.js' }
+        const uri = encodeURI(
+            `https://api.github.com/users/${ req.params.username }/repos?per_page=5&sort=created:asc`
+        )
+        const headers = {
+            'user-agent': 'node.js',
+            Authorization: `token ${ config.get('githubToken') }`
         }
-        request(options, (error, response, body) => {
-            if (error) {
-                console.error(error)
-            }
-            if (response.statusCode !== 200) {
-                res.status(404).json({ msg: 'No Github profile found' })
-            }
-            res.json(JSON.parse(body))
-        })
+        const githubResponse = await axios.get(uri, { headers })
+        return res.json(githubResponse.data)
     } catch (error) {
         console.error(error.message)
         res.status(500).send('Server error')
