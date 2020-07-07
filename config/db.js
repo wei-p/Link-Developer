9421b16694644d0468fa032a459beae467cf28d0
Wei Pan <wp2251@columbia.edu>
2020-07-07 00:21:22 -0400

    User API finished, Profile API in progress

--- config/db.js
+++ config/db.js
@@ -6,7 +6,9 @@ const connectDB = async () => {
     try {
         await mongoose.connect(db, {
             useNewUrlParser: true,
-            useUnifiedTopology: true
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+            useFindAndModify: true
         })
         console.log('MongoDB connected')
     } catch (err) {
